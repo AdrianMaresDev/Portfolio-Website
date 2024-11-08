aec32e7f1a0cad037a27897195d7d91df6ca8f28
AdrianMares <adrianmares.dev@gmail.com>
2024-11-07 21:57:56 -0500

    added scroller animation

--- main.js
+++ main.js
@@ -18,3 +18,26 @@ function lightModeToggle() {
         lightToggle.classList.remove('hidden');
     }
 }
+
+//Skill Icon Scroller
+
+const scrollers = document.querySelectorAll('.scroller');
+
+if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    scrollers.forEach((scroller) => {
+        scroller.setAttribute('data-animated', true);
+
+        const scrollerInner = scroller.querySelector('.scroller-inner');
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach(item => {
+            const duplicated = item.cloneNode(true);
+            duplicated.setAttribute('aria-hidden', true);
+            scrollerInner.appendChild(duplicated);
+        });
+    });
+}
